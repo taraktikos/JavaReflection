4661547564f45c900009a349680b0916cecf86c0
tsuhovenko <tsuhovenko@cogniance.com>
2014-11-21 17:32:34 +0200

    test

--- src/main/java/Implementor.java
+++ src/main/java/Implementor.java
@@ -1,3 +1,5 @@
+import com.sun.org.apache.xpath.internal.operations.Mod;
+
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -24,7 +26,111 @@ public class Implementor {
         }
     }
 
+    private String parametersToString(TypeVariable[] typeVariables) {
+        StringBuilder result = new StringBuilder();
+        if (typeVariables.length > 0) {
+            result.append("<");
+            for (int i = 0; i < typeVariables.length; i++) {
+                result.append(typeVariables[i].toString());
+                if (i != typeVariables.length - 1) {
+                    result.append(", ");
+                }
+            }
+            result.append(">");
+        }
+        return result.toString();
+    }
+
+    private String generateMethods() {
+        StringBuilder result = new StringBuilder();
+        //todo recursion
+        for (Method method: classEntity.getDeclaredMethods()) {
+            result.append("\t");
+            result.append(Modifier.toString(method.getModifiers()).replace(" abstract", ""));
+            result.append(" ");
+            String typeParameters = parametersToString(method.getTypeParameters());
+            if (typeParameters.length() > 0) {
+                typeParameters += " ";
+            }
+            result.append(typeParameters);
+            result.append(method.getGenericReturnType().toString().replace("[Ljava.lang.Object;", "java.lang.Object[]").replace("class ", ""));
+            result.append(" ");
+            result.append(method.getName());
+            result.append("(");
+            Type[] types = method.getGenericParameterTypes();
+            for (int i = 0; i < types.length; i++) {
+                result.append(types[i].toString().replace("class ", ""));
+                result.append(" arg");
+                result.append(i);
+                if (i != types.length - 1) {
+                    result.append(", ");
+                }
+            }
+            result.append(") ");
+            Class<?>[] exceptionTypes = method.getExceptionTypes();
+            if (exceptionTypes.length > 0) {
+                result.append("throws ");
+                for (int i = 0; i < exceptionTypes.length; i++) {
+                    result.append(exceptionTypes[i].toString());
+                    if (i != types.length - 1) {
+                        result.append(", ");
+                    }
+                }
+                result.append(" ");
+            }
+            result.append("{\n");
+            result.append("\t\treturn");
+            if (!method.getReturnType().equals(Void.TYPE)) {
+                if (method.getReturnType().equals(Boolean.TYPE)) {
+                    result.append(" false");
+                } else if (method.getReturnType().isPrimitive()) {
+                    result.append(" 0");
+                } else {
+                    result.append(" null");
+                }
+            }
+            result.append(";");
+            result.append("\n\t}\n");
+//            if (Modifier.isAbstract(method.getModifiers())) {
+//
+//            } else {
+//
+//            }
+        }
+        return result.toString();
+    }
+
+    private void generateClass() {
+        TypeVariable[] typeVariables = classEntity.getTypeParameters();
+        StringBuilder result = new StringBuilder("public class " + classEntity.getSimpleName() + "Impl");
+        String types = parametersToString(typeVariables);
+        result.append(types);
+        result.append(" ");
+        result.append(classEntity.isInterface() ? "implements" : "extends");
+        result.append(" ");
+        result.append(classEntity.getCanonicalName());
+        result.append(types);
+        result.append(" {\n");
+        result.append(generateMethods());
+
+
+        result.append("}");
+        //System.out.println(result);
+        PrintWriter writer = null;
+        try {
+            String path = "./src/main/java/" + classEntity.getSimpleName() + "Impl.java";
+            File f = new File(path);
+            f.createNewFile();
+            writer = new PrintWriter(path);
+            writer.print(result);
+            writer.flush();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
     public void print() {
+        generateClass();
         String className = p.matcher(classEntity.getName()).replaceAll("");
         String newClassName = className + "Impl";
         if (classEntity.isInterface()) {
@@ -54,17 +160,7 @@ public class Implementor {
         result.append("}");
         System.out.println(result);
 
-        PrintWriter writer = null;
-        try {
-            String path = "./target/" + className + "Impl.java";
-            File f = new File(path);
-            f.createNewFile();
-            writer = new PrintWriter(path);
-            writer.print(result);
-            writer.flush();
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
-        }
+
 
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,12 @@
-
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
+        List<Integer> list = new ListImpl<>();
         Class c;
         try {
-            c = Class.forName("java.util.List");
+            c = Class.forName("java.util.AbstractList");
         } catch (ClassNotFoundException e) {
             System.out.println("Class not found");
             return;
